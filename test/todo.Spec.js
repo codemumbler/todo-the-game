b780453c13b09010798749ccb38941507c4668ee
Steven Armstrong <codemumbler@users.noreply.github.com>
2015-06-07 20:03:44 -0400

    cleaned up the code a little

--- test/todo.Spec.js
+++ test/todo.Spec.js
@@ -1,36 +1,33 @@
 describe('TodoListController', function() {
 	beforeEach(module('todoApp'));
 
-	var $controller;
+	var $controller, controller;
 
 	beforeEach(inject(function(_$controller_){
 		// The injector unwraps the underscores (_) from around the parameter names when matching
 		$controller = _$controller_;
+		controller = $controller('TodoListController', { });
 	}));
 
 	describe('$controller.addTodo', function() {
 		it('add todo', function() {
-			var controller = $controller('TodoListController', { });
 			controller.todoText = 'abcdefghijklmnopqrstuvwxyz';
-			controller.todos = [ ];
 			controller.addTodo();
 			expect(controller.todos).toEqual([{text:'abcdefghijklmnopqrstuvwxyz', done:false}]);
 		});
 		
 		it('add empty does nothing', function() {
-			var controller = $controller('TodoListController', { });
 			controller.todoText = '';
-			controller.todos = [ ];
 			controller.addTodo();
 			expect(controller.todos).toEqual([]);
+			expect(controller.todoText).toEqual('');
 		});
 		
 		it('add empty trimmed does nothing', function() {
-			var controller = $controller('TodoListController', { });
 			controller.todoText = ' \t\n';
-			controller.todos = [ ];
 			controller.addTodo();
 			expect(controller.todos).toEqual([]);
+			expect(controller.todoText).toEqual('');
 		});
 	});
 });
